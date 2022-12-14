aa22e1aaab56305d48044f67fb1ab67610bbb33e
samuelkassa <samuelkassahun22@gmail.com>
2022-12-13 22:36:11 -0800

    10th commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * Description: prints a number in nine by nine table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int row, col, product, tens, ones;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = col * row;
+			tens = product / 10;
+			ones = product % 10;
+			if (col == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+
+	}
+}
