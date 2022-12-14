bad193037f489a004cd7f360cf22b8a191aab19a
samuelkassa <samuelkassahun22@gmail.com>
2022-12-13 20:23:30 -0800

    9th commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,13 +19,13 @@ void jack_bauer(void)
 			hours_max = 4;
 
 		hours_ones = 0;
-		while (hours_ones < 4)
+		while (hours_ones < hours_max)
 		{
 			minutes_tens = 0;
 			while (minutes_tens < 6)
 			{
 				minutes_ones = 0;
-				while (minutes_ones < 9)
+				while (minutes_ones < 59)
 				{
 					_putchar(hours_tens);
 					_putchar(hours_ones);
