452ce349f0588a34be31f6b263fbee85ddb83840
samuelkassa <samuelkassahun22@gmail.com>
2022-12-14 00:07:38 -0800

    13th commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,7 +35,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) % 10 + '0');
-					_putchar(prdouct % 10 + '0');
+					_putchar(product % 10 + '0');
 				}
 				else if (product < 1000 && product >= 100)
 				{
