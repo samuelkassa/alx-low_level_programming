f635abe75e22f91b769142e27b6029b74bf1a516
samuelkassa <samuelkassahun22@gmail.com>
2022-12-14 00:11:06 -0800

    13th commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,11 +12,11 @@ void print_times_table(int n)
 {
 	int col, row, product;
 
-	if (n <= 15 && n >= 0)
+	if (n < 15 && n >= 0)
 	{
-		for (row = 0; row <= 15; row++)
+		for (row = 0; row <= n; row++)
 		{
-			for (col = 0; col <= 15; col++)
+			for (col = 0; col <= n; col++)
 			{
 				product = row * col;
 				if (col == 0)
