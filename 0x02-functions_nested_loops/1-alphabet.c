1f430a0205ea74507eae16fb2dbb3810631043fd
samuelkassa <samuelkassahun22@gmail.com>
2022-12-13 18:30:08 -0800

    Second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints alphabet
+ *
+ * Return: Always 0 (successfull)
+ */
+void main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n);
+}
