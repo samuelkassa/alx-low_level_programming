f687bfa16636460d641f75e517bbf14d966e7bc5
samuelkassa <samuelkassahun22@gmail.com>
2022-12-13 19:37:10 -0800

    8th commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "man.h"
+
+/**
+ * print_last_digit - Entry point
+ * Description: prints the last digit
+ * @n: integer
+ *
+ * Return: integer
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	if (n < 0)
+		last = last * -1;
+
+	_putchar(last + '0);
+
+	return (last);
+}
