15a488dfb39fb056e7da79d05baccda8c15b88b7
samuelkassa <samuelkassahun22@gmail.com>
2022-12-15 10:34:12 -0800

    printing numbers between 0 and 9 with exception of 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != '2'  && c != 4)
+		if (c != '2'  && c != '4')
 		{
 			_putchar(c);
 		}
@@ -20,7 +20,6 @@ void print_most_numbers(void)
 		{
 			continue;
 		}
-		c++;
 	}
 	_putchar('\n');
 }
