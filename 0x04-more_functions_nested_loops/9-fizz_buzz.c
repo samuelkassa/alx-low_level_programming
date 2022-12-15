a151fa9382661d1385a8d9dff3981c28c4d248ea
samuelkassa <samuelkassahun22@gmail.com>
2022-12-15 13:59:06 -0800

    printing numbers fizzbuzz fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry point
  * Description: it prints number, fizz, buzz or fizzBuzz
@@ -15,21 +15,20 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-		printf(" ");
 	}
 	printf("\n");
 }
