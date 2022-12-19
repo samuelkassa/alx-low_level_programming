883cf728d8801480f93347bf469810e51e5bc3b9
samuelkassa <samuelkassahun22@gmail.com>
2022-12-19 14:22:45 -0800

    Printing the last half of the string characters

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the last half of the string
+ * @str: pointer to char type
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+	}
+	if (i % 2 == 0)
+	{
+		j = i / 2;
+	}
+	else
+	{
+		j = (i - 1) / 2;
+	}
+	while (j <= i)
+	{
+		_putchar(j);
+		j++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-Printing charactres of a string with even index number
+Printing half of the string
