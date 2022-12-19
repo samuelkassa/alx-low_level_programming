da542fabed0f1ac68ea0e02fcce2d5df35070e75
samuelkassa <samuelkassahun22@gmail.com>
2022-12-19 14:49:37 -0800

    Printing the last half of the string characters

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,19 +11,11 @@ void puts_half(char *str)
 {
 	int i, j;
 
-	for (i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
-	if (i % 2 == 0)
-	{
-		j = i / 2;
-	}
-	else
-	{
-		j = (i - 1) / 2;
-	}
-
-	while (str[j] != 0)
+	j = i / 2;
+	while (j <= i)
 	{
 		_putchar(str[j]);
 		j++;
