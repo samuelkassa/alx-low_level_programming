f8084bd7fe253c5e89888a452926826ebcd562ad
samuelkassa <samuelkassahun22@gmail.com>
2022-12-21 12:36:54 -0800

    Concatinating two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,14 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; src[j]; j++)
+	for (j = 0; j < i && src[j] != '\0'; j++)
 	{
-		src[i + j] = dest[j];
+		dest[i + j] = src[j];
 	}
-	src[i + j] = '\0';
-	_putchar('\n');
+	dest[i + j] = '\0';
 
 	return (src);
 }
