bdeb34f412df01663a3ae54fb77a469abaf4a828
samuelkassa <samuelkassahun22@gmail.com>
2022-12-21 16:22:19 -0800

    comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ *
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, remainder;
+
+	for (i = 0, j = 0; s1[i] != '\0' && s2[j] != '\0'; i++, j++)
+	{
+		if (_putchar(s1[i]) == _putchar(s2[j]))
+		{
+			remainder = _putchar(s1[i]) - _putchar(s2[j]);
+			return (remainder);
+		}
+		else if (_putchar(s1[i]) > _putchar(s2[j]))
+		{
+			remainder = _putchar(s1[i]) - _putchar(s2[j]);
+			return (remainder);
+		}
+		else
+		{
+			remainder = -(_putchar(s1[i]) - _putchar(s2[j]));
+			return (remainder);
+		}
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0. Concatinating two strings
 1. concatinating two strings based on integer n parameter
 2. Copying src text to dest based on n value
+3. Comparing two texts which is equivalent to strcmp
