8312ff4e05a9d88f59a4fc8c40e85f37b2a1dbab
samuelkassa <samuelkassahun22@gmail.com>
2022-12-22 05:45:53 -0800

    Changing characters to upper case based on the preceding characters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,7 +24,7 @@ char *cap_string(char *str)
 		str[str_index - 1] == '!' || str[str_index - 1] == '?' ||
 		str[str_index - 1] == '"' || str[str_index - 1] == '(' ||
 		str[str_index - 1] == ')' || str[str_index - 1] == '{' ||
-		str[str_index - 1] == '}')
+		str[str_index - 1] == '}' || str_index == 0)
 			str[str_index] -= 32;
 
 		str_index++;
