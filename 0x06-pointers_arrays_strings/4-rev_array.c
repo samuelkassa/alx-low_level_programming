081f852da21397aad47cc9537c2a70b304663f31
samuelkassa <samuelkassahun22@gmail.com>
2022-12-21 17:41:29 -0800

    Rerersing the content of array integer

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses the elements of integer array
+ * @a: array
+ * @n: number of element of array a
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int left, right, middle;
+
+	for (i = 0; i < middle; i++)
+	{
+		left = a[n - i];
+		right = a[i];
+		a[n - i] = right;
+		a[i] = left;
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1. concatinating two strings based on integer n parameter
 2. Copying src text to dest based on n value
 3. Comparing two texts which is equivalent to strcmp
+4. reversing the content of an integer array
