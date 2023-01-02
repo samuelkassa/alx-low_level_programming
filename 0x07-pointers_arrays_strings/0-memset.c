f33c574205f0aedd67b1578d1d187997b66c0ea3
samuelkassa <samuelkassahun22@gmail.com>
2023-01-02 00:45:51 -0800

    Manipulating memory content

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset -  function fills the first n bytes of the memory area pointed to b
+ * y s with the constant byte b.
+ * @s: pointer area in which value are going to be printed
+ * @b: constant which is going to printed
+ * @n: byte size
+ *
+ * Return: pointer to the memory size area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		s[i]= b;
+	}
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+Even more pointers, arrays and strings
+1. A function that fills memory with a constant byte.
+	The _memset() function fills the first n bytes of the memory.
+	area pointed to by s with the constant byte b.
+	Returns a pointer to the memory area s.
+2. A function that copies memory area.
+	The _memcpy() function copies n bytes from memory area src to memory area dest.
+	Returns a pointer to dest.
