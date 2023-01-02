6664a5f4b360cd80c513ad60932a77044f920c19
samuelkassa <samuelkassahun22@gmail.com>
2023-01-02 09:50:52 -0800

    copying files

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char destination = dest, source = src;
+	char *destination = dest, *source = src;
 
 	for (i = 0; i < n; i++)
 	{
