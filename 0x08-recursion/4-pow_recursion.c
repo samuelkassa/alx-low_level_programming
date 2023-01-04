d331f819e4278742c7be3991c9afaadb375d1574
samuelkassa <samuelkassahun22@gmail.com>
2023-01-04 13:27:10 -0800

    Recursive power function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/**_pow_recursion - a function that returns the value of x raised to th
- * e power of y.
+/**
+ * _pow_recursion - a function that returns the value of x raised to
+ * the power of y.
  * @x: base integer.
  * @y: exponent integer
  *
@@ -12,8 +13,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (y = 0)
+	if (y == 0)
 		return (1);
-	return x *= x;
-	_po_recursion(x, y-1);
+	return (x *= x);
+	_pow_recursion(x, y - 1);
 }
