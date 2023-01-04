e9d8be46f80156d59d3b82f369685362a978be42
samuelkassa <samuelkassahun22@gmail.com>
2023-01-04 08:54:38 -0800

    Printing string characters

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a stirng, followed by a new 
+ * line.
+ * @s: A string to be printed.
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+		
+}
