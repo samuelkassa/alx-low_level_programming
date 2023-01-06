d3b47d52010d27dc8ade962cf54bb2da2d45116d
samuelkassa <samuelkassahun22@gmail.com>
2023-01-06 11:56:57 -0800

    printing program name followed by a newline

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,9 @@
  * Return: Always 0.
  */
 
-int main(int _attribute_((_unused_)) argc, char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
 	return (0);
 }
-		
