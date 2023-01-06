bbcc8dbccb19c54894b69413510a079ce0f665d7
samuelkassa <samuelkassahun22@gmail.com>
2023-01-06 14:22:19 -0800

    Printing all memebers of argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints all the arguments
+ * @argc: The number of arguments
+ * @argv: Arguments Vector
+ *
+ * Return: Always 0 success
+ */
+
+int main (int argc, cha *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
