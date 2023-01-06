2d584c9fa9538c6fa1509e910dbff27b35b7a177
samuelkassa <samuelkassahun22@gmail.com>
2023-01-06 14:39:18 -0800

    Printing the multiplication two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the multiplication of two numbers
+ * @argc: Argument counter
+ * @argv: Argument vector
+ *
+ * Return: if two numbers are inserted then the result will be printed
+ * If not Erro will be printed and 1 will be returned.
+ */
+
+int main(int argc, char *argv[])
+{
+	int value1 = 0;
+	int value2 = 0;
+
+	if (argc == 3)
+		value1 = atoi(argv[1]);
+		value2 = atoi(argv[2]);
+		printf("%d\n", value1 * value2;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
