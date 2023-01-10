5e9365ad4439da7c50e1a10925d2321424fbf14f
samuelkassa <samuelkassahun22@gmail.com>
2023-01-09 18:31:57 -0800

    Concatenating two strings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		for (i = 0; i < sizeof(s1); i++)
 		{
-			*(str + i) = *(s + i);
+			*(str + i) = *(s1 + i);
 		}
 		for (j = 0; j < sizeof(s2); j++)
 		{
