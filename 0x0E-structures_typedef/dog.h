7ba2cf5936d2353faf48ea8fd76ac28c2848fcc0
samuelkassa <samuelkassahun22@gmail.com>
2023-01-16 06:43:48 -0800

    Sturcture for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,11 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Author: Samuyel Kasahun
+ * Description: This is a heder file that defines a new type struct dog..
+ */
 
 /**
  * struct dog - a structure about dog.
@@ -15,3 +22,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ *  dog_t - typedef for struct dog.
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
