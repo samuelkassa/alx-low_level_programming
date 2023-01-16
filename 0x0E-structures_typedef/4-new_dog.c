6c0e2bd52b753c0dd907828b8b306792a8f326f1
samuelkassa <samuelkassahun22@gmail.com>
2023-01-16 10:35:40 -0800

    Creating a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -71,8 +71,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-
-	malloc->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	madog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 
 	if (madog == NULL)
 	{
