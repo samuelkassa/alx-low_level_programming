5600327bb5c7c45b14d1c52d5c79205c9ed0b013
samuelkassa <samuelkassahun22@gmail.com>
2023-01-18 11:27:00 -0800

    Printing array elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointer.h"
+
+/**
+ * arraay_iterator - a function that executes a function given as a parameter
+ * on each element of an array.
+ * @size is the size of the array.
+ * @action : is a pionter to the function you need to use
+ * @array: is the array.
+ * @size_t : is the type of the sie.
+ *
+ *Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
