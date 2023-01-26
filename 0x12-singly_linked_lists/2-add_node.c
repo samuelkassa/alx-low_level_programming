92c9539acf85a5f0f984009d6dc19d00dd999a65
samuelkassa <samuelkassahun22@gmail.com>
2023-01-26 14:12:47 -0800

    Adding new element to a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,7 +17,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node != NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	dup = strdup(str);
