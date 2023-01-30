e23c9379d19139936e31c96a6bb5af24309ad9c2
samuelkassa <samuelkassahun22@gmail.com>
2023-01-30 09:49:06 -0800

    Update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,6 +15,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	traverse = *head;
 	ending = malloc(sizeof(listint_t));
 
+	if (ending == NULL)
+		return (NULL);
 	ending->n = n;
 	ending->next = NULL;
 
