27bead1714d14dbc686c49648f952a8ac181ec0c
samuelkassa <samuelkassahun22@gmail.com>
2023-01-30 16:21:22 -0800

    Adding all nodes data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Summing up all the data (n) of a listint_t
+ * linked list
+ * @head: A pointer to the head of listint_t struct
+ *
+ * Return: Sum of all the data (n)
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+
+	while (head)
+	{
+		sum = head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
