116792f1b44538de464778c93682159cb065f411
samuelkassa <samuelkassahun22@gmail.com>
2023-01-31 05:47:14 -0800

    Reversing struct nodes

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - A function that reverses a listint_t linked list.
+ * @head: A pointer of pointer to head of a struct.
+ *
+ * Return: Pointer to first element in a Reversed struct
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *right,, *left;
+
+	if ((*head == NULL) || (head == NULL))
+		return (NULL);
+
+	while ((*head)->next)
+	{
+		right = (*head)->next;
+		(*head)->next = left;
+		left = *head;
+		*head = right;
+	}
+
+	(*head)->next = left;
+
+	return (*head);
+}
