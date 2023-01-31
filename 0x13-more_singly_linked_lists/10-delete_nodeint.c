77213d4e710ef4bec13f63ad33e1b976c5012d67
samuelkassa <samuelkassahun22@gmail.com>
2023-01-30 18:09:59 -0800

    Removing a node at a specified location

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		traverse = traverse->next;
 	}
 	tmp = traverse->next;
-	traverse = tmp->next;
+	traverse->next = tmp->next;
 	free(tmp);
 	return (1);
 }
