39a639ea773fc37cc754925b45be704e9fdb3c37
samuelkassa <samuelkassahun22@gmail.com>
2023-02-02 14:25:57 -0800

    Finding the binary value at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * git_bit - finds the binary value of a number at a given index.
+ * @n: The given number.
+ * @index: The index for checking the value.
+ *
+ * Return: Value of the bit at index index or -1 if an error occured.
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int ) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
