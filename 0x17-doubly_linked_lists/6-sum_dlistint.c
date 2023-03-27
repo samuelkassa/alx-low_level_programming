47dc8920443caf56c297f0add81221ce0c68c6d1
samuelkassa <samuelkassahun22@gmail.com>
2023-03-27 16:02:36 -0700

    adding all elements of the doubly linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,10 +11,12 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
+	if (head == NULL)
+		return (0);
 	while (head)
 	{
 		sum += head->n;
-		head = head->next
+		head = head->next;
 	}
 	return (sum);
 }
