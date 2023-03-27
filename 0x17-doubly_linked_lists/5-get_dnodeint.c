46b4ac82296dd44de4cedb5d81825c4fb17358f1
samuelkassa <samuelkassahun22@gmail.com>
2023-03-27 14:40:20 -0700

    Returnin the nth node in a doubly linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,9 +13,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	for (; index != 0; index--)
 	{
-		if (head = NULL)
-			return NULL;
-		head = head-next;
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
 	}
 	return (head);
 }
