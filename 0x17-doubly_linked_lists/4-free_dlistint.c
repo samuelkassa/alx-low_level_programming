282d7c5237c464e51c4a2a793e863cb7cbb78524
samuelkassa <samuelkassahun22@gmail.com>
2023-03-27 13:45:44 -0700

    freeing a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a given linked list
+ * @head: The head to list
+ *
+ * Return: None
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t * temp;
+
+	while(head->next)
+	{
+		printf("%d", head->n);
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
